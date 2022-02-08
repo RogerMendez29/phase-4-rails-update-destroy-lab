8f91a33db2d33221a807aad7826f02cf3e03d05e
Roger Mendez <roger61087@gmail.com>
2022-02-08 14:52:02 -0500

    commit tree 0d63369ac1007ab2057e70d75d25319d3f4389fc

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,7 @@
 class PlantsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   # GET /plants
   def index
     plants = Plant.all
@@ -8,7 +10,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -18,7 +20,28 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+  end
+  
+
   private
+
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
+  end
+
+  def find_plant
+    Plant.find_by(id: params[:id])
+  end
+  
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
